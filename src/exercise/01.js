70e3cd075c5235521df1ffe73161326d4e1f6cc7
“Hanna <hanna.abrahamsson@forefront.se>
2023-01-02 15:20:55 +0100

    updated to arrow function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,21 +3,21 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+const Greeting = ({initialName = ''}) => {
   // 💣 delete this variable declaration and replace it with a React.useState call
   const [name, setName] = React.useState(initialName)
   
 
-  function handleChange(event) {
+  function handleChangeName(event) {
     // 🐨 update the name here based on event.target.value
-    setName(event.target.value)
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChangeName} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
