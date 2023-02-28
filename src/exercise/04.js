0892d0e29c7919584c9371690d6583e0cb35dcd7
Hanna Abrahamsson <hanna.abrahamsson@forefront.se>
2023-02-28 17:12:07 +0100

    finishes ex 04 extra credit 3

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,69 +2,12 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-const Board = () => {
-  // 🐨 squares is the state for this component. Add useState for squares
-
-  const stickySquares = JSON.parse(window.localStorage.getItem('squares'));
-
-  const [squares, setSquares] = React.useState(() => stickySquares || Array(9).fill(null))
-
-  React.useEffect(() => {
-    window.localStorage.setItem('squares', JSON.stringify(squares));
-  }, [squares]);
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  const nextValue = calculateNextValue(squares);
-
-  const winner = calculateWinner(squares);
-
-  const status = calculateStatus(winner, squares, nextValue);
-
-  //   
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  const selectSquare = (square) => {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-
-    if (winner || squares[square]) {
-      return
-    }
-
-    const squaresCopy = [...squares];
-    squaresCopy[square] = nextValue;
-    setSquares(squaresCopy)
-  }
-
-  const restart = () => {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-    setSquares(Array(9).fill(null));
-  }
-
+const Board = ({onClick, squares}) => { 
   const renderSquare = (i) => {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -72,8 +15,6 @@ const Board = () => {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -89,20 +30,67 @@ const Board = () => {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 const Game = () => {
+  const [currentStep, setCurrentStep] = useLocalStorageState("tictactoe:step", 0);
+  const [history, setHistory] = useLocalStorageState("tictactoe:history",[Array(9).fill(null) ]);
+
+  const currentSquares = history[currentStep];
+
+  const nextValue = calculateNextValue(currentSquares);
+  const winner = calculateWinner(currentSquares);
+  const status = calculateStatus(winner, currentSquares, nextValue);
+
+
+  const selectSquare = (square) => {
+    if (winner || currentSquares[square]) {
+      return
+    }
+
+    const newHistory = history.slice(0, currentStep + 1);
+    
+    const squaresCopy = [...currentSquares];
+    squaresCopy[square] = nextValue;
+
+    setHistory([...newHistory, squaresCopy])
+    setCurrentStep(newHistory.length)
+    
+  }
+
+  const restart = () => {
+    setHistory(Array(9).fill(null));
+    setCurrentStep(0);
+  }
+
+  const moves = history.map((stepSquares, step) => {
+    const description = step === 0 ? "Go to game start" : `Go to move ${step}`;
+    const isCurrentStep = step === currentStep
+    return (
+      <li key={`game-component__${step }`}>
+  <button
+   disabled={isCurrentStep} 
+   onClick={() => setCurrentStep(step)}>
+   {description} {isCurrentStep ? '(current)' : null}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
+    <div className="game-board">
+      <Board onClick={selectSquare} squares={currentSquares} />
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
     </div>
+    <div className="game-info">
+      <div>{status}</div>
+      <ol>{moves}</ol>
+    </div>
+  </div>
   )
 }
 
